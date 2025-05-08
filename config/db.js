42faaa700365c6db4a4340b3f06f43c7c03b253a
V-Satwik-Reddy <satwikstudies05@gmail.com>
2025-05-08 23:21:00 +0530

    added max poolsize for more improvance

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
     try{
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URI,{
+            maxPoolSize: 200
+        });
 
         console.log(`MongoDB connected : ${conn.connection.host}`)
     }
